7a8e8fc94708e7723a727405c2ad83dbf143f9bd
dareymorales55-ux <dareymorales55@gmail.com>
2026-06-22 23:51:51 -0700

    Implement WishSMP mod initializer with server reference

--- src/main/java/com/deejay/wishsmp/WishSMP.java
+++ src/main/java/com/deejay/wishsmp/WishSMP.java
@@ -1,1 +1,18 @@
+package com.deejay.wishsmp;
 
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.MinecraftServer;
+
+public class WishSMP implements ModInitializer {
+
+    private static MinecraftServer server;
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[WishSMP] Server Mod Loaded Successfully!");
+    }
+
+    public static MinecraftServer getServer() {
+        return server;
+    }
+}
